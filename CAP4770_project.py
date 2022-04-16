46739446a4841b4169ef9c6017872f186ee29d22
srsiegel <steven.siegel@ufl.edu>
2022-04-16 14:51:26 -0400

    View dates and results for traffic stop data

--- CAP4770_project.py
+++ CAP4770_project.py
@@ -2,3 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+#Louisville traffic stop data
+c=pd.read_csv("CSVs/LMPD_STOPS_DATA.csv")
+#Fayetteville traffic stop data
+d=pd.read_csv("CSVs/Traffic_Stops.csv")
+
+print(c[['ACTIVITY_DATE', 'ACTIVITY RESULTS']])
+print(d[['stopdate', 'eDesc']])
+
+
+
